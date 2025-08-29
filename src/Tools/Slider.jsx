88491f1a9e2a841211b8a857eb2fcf5c9f7a4031
JkuatDebates <kenanexon@gmail.com>
2025-08-29 16:21:49 +0300

    Carousel UI update
    -Touch swiping
    -Mouse drag swiping
    -Swiping fade animation
    -Home skill card animation bug fix
    -

--- src/Tools/Slider.jsx
+++ src/Tools/Slider.jsx
@@ -6,7 +6,36 @@ export default function Slider({slides, source}){
     const [currentSlide,setCurrentSlide]=useState(0);
     const [currentImageSlide,setCurrentImageSlide]=useState(0);
     const [view,setView]=useState(false);
+    let startX=0;
+    let endX=0;
 
+    function handleTouchStart(e){
+        startX=e.touches[0].clientX;
+    }
+    function handleTouchEnd(e){
+        endX=e.changedTouches[0].clientX;
+        swipe();
+    }
+    function handleMouseDown(e){
+        startX=e.clientX;
+    }
+    function handleMouseUp(e){
+        endX=e.clientX;
+        swipe();
+    }
+    function swipe(){
+        let l=endX-startX;
+        if(source!=='CAD'){
+            if(l>=30)
+                inext();
+            else if(l<=-30) iprev();
+        }
+        else{
+            if(l>=30)
+                next();
+            else if(l<=-30) prev();
+        }
+    }
     function prev(){
         if(currentSlide===0) setCurrentSlide(slides.length-1);
         else setCurrentSlide(currentSlide-1);
@@ -37,23 +66,33 @@ export default function Slider({slides, source}){
     return(
         <>{
             source==='CAD'?
-        <div className="carousel">
+        <div className="carousel" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
             
             <Lightbox image={slides[currentSlide].image} view={view} setView={setView}/>
             <button onClick={prev}><FcPrevious/></button>
-            <div>
-            <img src={slides[currentSlide].image} alt="Slide" onClick={()=>setView(!view)}/>
-            <div className="caption">{slides[currentSlide].text}</div>
-            </div> 
+
+            {slides.map((im,index)=>{
+                const active=index===currentSlide;   
+                return(
+                <div key={index} style={{display:active?'block':'none'}}>
+                <img src={im.image} alt="Slide" onClick={()=>setView(!view)}/>
+                <div className="caption">{im.text}</div>
+                </div>)})
+            }
+
             <button onClick={next}><FcNext/></button>         
         </div>
-        :<div className="carousel">
+        :<div className="carousel" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} >
             <Lightbox image={slides[currentSlide].image[currentImageSlide]} view={view} setView={setView}/>
             <button onClick={iprev}><FcPrevious/></button>
-            <div className="carouselImageFrame">
-            <img src={slides[currentSlide].image[currentImageSlide]} alt="Slide" onClick={()=>setView(!view)}/>
-            <div className="caption">{slides[currentSlide].text}</div>
-            </div> 
+            {slides[currentSlide].image.map((im,index)=>{
+                const active=index===currentImageSlide;   
+                return(
+                <div className="carouselImageFrame" key={index} style={{display:active?'flex':'none'}}>
+                <img src={im} alt="Slide" onClick={()=>setView(!view)}/>
+                <div className="caption">{slides[currentSlide].text}</div>
+                </div>)})
+            }
             <button onClick={inext}><FcNext/></button>         
         </div>}
         </>
