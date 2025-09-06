3c3f021a380630f8ff701c00a8761e55b6aeacbc
JkuatDebates <kenanexon@gmail.com>
2025-09-06 16:10:35 +0300

    Furniture and Cast Iron Juicer CAD designs

--- src/CAD.jsx
+++ src/CAD.jsx
@@ -6,6 +6,12 @@ export default function CAD(){
         {image:'/projects/realSpoon.jpg', text:'Real Spoon'},
         {image:'/projects/spoon2D.JPG', text:'2D Drawing'}
     ];
+    const furniture=[
+        {image:"/projects/Furniture/Sofa.JPG", text:'Sofa'},
+        {image:"/projects/Furniture/Brass ringed table.JPG", text:'Table 1'},
+        {image:"/projects/Furniture/Screw table.JPG", text:'Table 2'},
+        {image:"/projects/Furniture/Table Assembly.JPG", text:'Table 3'},
+    ]
     return(
         <>
         <section className="CADintro">
@@ -13,6 +19,22 @@ export default function CAD(){
             <p>The following CAD designs were made on SOLIDWORKS. I am familiar with SOLIDWORKS, AutoDesk Inventor and AutoCAD</p>
         </section>
         <section className="CADcards">
+            <div className="CADcard">
+                <div>
+                <h3>Cast Iron Juicer</h3>
+                <video src="/projects/CIJ Assembly.mp4" controls></video>
+                </div>
+                <div>
+                    <strong>Skills</strong>
+                    <ul>
+                        <li>Reverse Engineering</li>
+                        <li>Projection curves</li>
+                        <li>Assembly with mates</li>
+                        <li>Motion Study</li>
+                    </ul>
+                    <p>Inspired by Youtube shorts</p>
+                </div>
+            </div>
             <div className="CADcard">
                 <div>
                 <h3>Electrical Enclosure</h3>
@@ -74,6 +96,21 @@ export default function CAD(){
                         <li>SOLIDWORKS Toolbox</li>
                         <li>Exploded View</li>
                         <li>Motion study: motor, contacts</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="CADcard">
+                <div>
+                <h3>Furniture</h3>
+                <Slider slides={furniture} source='CAD'/>
+                </div>
+                <div>
+                    <strong>Skills</strong>
+                    <ul>
+                        <li>Surface Modelling</li>
+                        <li>Lofting</li>
+                        <li>Revolve</li>
+                        <li>Adding Material</li>
                     </ul>
                 </div>
             </div>
